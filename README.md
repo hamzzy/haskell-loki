58a1551aba66bdeff03b1ef0951a0cdc8bb4f7cb
ibrahim Hamzat <hamat.ibrahim3@gmail.com>
2024-03-31 23:47:25 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# haskell-loki
+# Haskell Loki Logger
+
+Haskell Loki Logger Client is a library for sending logs from Haskell applications to Loki, With this library, you can seamlessly integrate Loki logging capabilities into your Haskell applications.
+
+## Features
+
+- **Simple Integration**: Easily integrate Loki logging into your Haskell applications with just a few lines of code.
+- **Asynchronous Logging**: Send logs to Loki asynchronously to avoid blocking your application's execution.
+- **Customizable Configuration**: Configure Loki logging settings, such as endpoint URL, labels, and log level filtering, to suit your application's requirements.
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,85 @@
 module Lib
-    ( someFunc
+    ( errorLog,infoLog,warnLog
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Exception (throwIO)
+import Data.Aeson (ToJSON,FromJSON, encode, object)
+import Control.Lens ((.~), (^.), (^?),(?~))
+import Data.Aeson
+import Data.Aeson.Types (Value(..))
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy.Char8 as BS
+import Data.Maybe (Maybe, fromMaybe)
+import Data.Text
+import Data.Aeson.Lens 
+import Data.Time.Clock (getCurrentTime)
+import GHC.Generics (Generic)
+import Network.Wreq
+import Control.Monad
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.Aeson.Types (Pair)
+import Relude hiding (ByteString,encodeUtf8, ask)
+import Data.Text.Encoding
+import Effectful.Reader.Static (ask)
+import Data.Aeson (encode, object, (.=))
+
+data LogData = LogData
+  { streams :: [Stream]
+  }
+  deriving (Show, Generic)
+
+data Stream = Stream
+  { stream :: StreamInfo
+  , values :: [[String]]
+  }
+  deriving (Show, Generic)
+
+data StreamInfo = StreamInfo
+  { label :: String
+  }
+  deriving (Show, Generic)
+
+instance ToJSON LogData
+instance FromJSON LogData
+instance ToJSON Stream
+instance FromJSON Stream
+instance ToJSON StreamInfo
+instance FromJSON StreamInfo
+
+callApi :: String -> String -> Env -> IO ()
+callApi level message tsEnv = do
+  now <- liftIO getCurrentTimeInNanoseconds
+  let nowText = pack . show $ now
+      messageText = pack message
+      logData = LogData
+        [ Stream
+            (StreamInfo (level))
+            [ [""]
+            , [""]
+            ]
+        ]
+      logDataJson = BS.unpack $ encode logData
+      opts = defaults
+        & auth ?~ basicAuth (encodeUtf8 $ tsEnv.lokiUsername) (encodeUtf8 $ tsEnv.lokiPassword)
+        & header "Content-Type" .~ ["application/json"]
+  resp <- postWith opts (toString $ tsEnv.lokiUrl) (BS.pack logDataJson)
+  print (resp ^. responseStatus)
+
+
+customLevel :: String ->  String -> TalstackEnv -> IO ()
+customLevel level message tsEnv = callApi level message  tsEnv
+
+errorLog :: String -> TalstackEnv -> IO ()
+errorLog message tsEnv = customLevel "error" message tsEnv
+
+infoLog :: String -> TalstackEnv -> IO ()
+infoLog message tsEnv = customLevel "info"  message tsEnv
+
+warnLog :: String -> TalstackEnv -> IO ()
+warnLog message tsEnv = customLevel "warn"  message tsEnv
+
+debugLog :: String -> TalstackEnv -> IO ()
+debugLog message tsEnv = customLevel "debug" message tsEnv
